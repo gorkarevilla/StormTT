4d6c16805d8ca31c8775b583ddb7f22cf2ae38c5
Gorka Revilla <gorkarevilla@gmail.com>
2016-11-17 09:28:12 +0100

    Added Emit next to the LangBolt

--- StormTT/src/main/java/master2016/LangBolt.java
+++ StormTT/src/main/java/master2016/LangBolt.java
@@ -11,6 +11,7 @@ import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichBolt;
 import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
 
 /**
  * Bolt Filter to catch only the languages needed.
@@ -26,15 +27,18 @@ public class LangBolt extends BaseRichBolt {
 	 */
 	private static final long serialVersionUID = 5259021092740992581L;
 	
-	
+	//Language to be filter
 	private final String lang;
+	
+	//Collector
+	private OutputCollector collector;
 
 	public LangBolt(String lang) {
 		this.lang = lang;
 	}
 
 	public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
-		// NOTHING TO PREPARE
+		this.collector = collector;
 
 	}
 
@@ -51,6 +55,13 @@ public class LangBolt extends BaseRichBolt {
 				System.out.println("LangBolt"+this.lang+"=> Language: " + tupleLang + " Hashtag: "+tupleHashtag);
 
 		}
+		
+		//Send to the next only the hashtag
+		collector.emit(input,new Values(tupleHashtag));
+		//Confirm received
+		collector.ack(input);
+		
+		
 	}
 
 	public void declareOutputFields(OutputFieldsDeclarer declarer) {
